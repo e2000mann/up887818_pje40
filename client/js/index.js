224e769f7e9fa37c04bc53bd7fb9087d7151238a
up887818 <57417766+up887818@users.noreply.github.com>
2021-01-06 13:41:41 +0000

    update 6 jan
    - created functions to get data from directory.json into dom. need to create functions to load lesson & quiz pages now
    - fixed readme hopefully

--- client/js/index.js
+++ client/js/index.js
@@ -5,8 +5,56 @@ async function loadTopics(){
   // sends request to server to retrieve directory.json
   let url = '/loadDir';
   let response = await fetch(url);
-  let directory = await response.json;
+  let directory = await response.json();
   console.log(directory);
+  let topics = directory.topics;
+
+  // gets template from index.html
+  const topicSection = document.querySelector("#topicSection");
+
+  // special case for 1st topic only - no quiz available
+  addSpecialTopicSection(topics[0]);
+  // removes 1st topic as topic section created
+  topics.shift();
+
+  topics.forEach(addTopicSection);
+}
+
+// special function for 1st topic only
+function addSpecialTopicSection(value){
+  // copy template content (inc. children)
+  let clone = topicSection.content.cloneNode(true);
+
+  // change text using value
+  // output = "n) Topic name", where n is topic id
+  let text = `${value.id}) ${value.name}`;
+  // text box is 1st p tag in clone
+  let textBox = clone.querySelectorAll("p")[0];
+  textBox.textContent = text;
+
+  // remove quiz button as not necessary (2nd button tag in clone)
+  let quizButton = clone.querySelectorAll("button")[1];
+  console.log(quizButton);
+  quizButton.remove();
+
+  // add clone to DOM
+  document.body.appendChild(clone);
+}
+
+// function for rest of topics
+function addTopicSection(value){
+  // copy template content (inc. children)
+  let clone = topicSection.content.cloneNode(true);
+
+  // change text using value
+  // output = "n) Topic name", where n is topic id
+  let text = `${value.id}) ${value.name}`;
+  // text box is 1st p tag in clone
+  let textBox = clone.querySelectorAll("p")[0];
+  textBox.textContent = text;
+
+  // add clone to DOM
+  document.body.appendChild(clone);
 }
 
 //loads topics when window is loaded
